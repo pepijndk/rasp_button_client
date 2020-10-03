25f5a3fd2325a2f23486149f4849f00948f66371
Pepijn de Kruijff <p.g.m.dekruijff@student.tudelft.nl>
2020-10-03 21:08:01 +0200

    added feedback light class

---
+++ feedback_led.py
@@ -0,0 +1,48 @@
+# importeer de GPIO bibliotheek.
+import RPi.GPIO as GPIO
+
+#
+# Constants
+#
+
+# GPIO pins
+PIN_LED_RED = 27  # Red led
+PIN_LED_BLUE = 22  # Red led
+PIN_LED_GREEN = 17  # Red led
+
+
+#
+# Configuration
+#
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_LED_RED, GPIO.OUT)
+GPIO.setup(PIN_LED_BLUE, GPIO.OUT)
+GPIO.setup(PIN_LED_GREEN, GPIO.OUT)
+
+
+#
+# Vars
+#
+
+# 0 = running script, 1 = listening but not connected, 2 = running and connected
+status = 0
+
+#
+# Functions
+#
+
+
+def setStatus(status: int):
+    GPIO.output(PIN_LED_RED, GPIO.LOW)
+    GPIO.output(PIN_LED_GREEN, GPIO.LOW)
+    GPIO.output(PIN_LED_BLUE, GPIO.LOW)
+    if status == 0:
+        GPIO.output(PIN_LED_BLUE, GPIO.HIGH)
+    if status == 1:
+        GPIO.output(PIN_LED_RED, GPIO.HIGH)
+    if status == 2:
+        #GPIO.output(PIN_LED_GREEN, GPIO.HIGH)
+
+        # because green is not working in this led, activate blue again:
+        GPIO.output(PIN_LED_BLUE, GPIO.HIGH)
