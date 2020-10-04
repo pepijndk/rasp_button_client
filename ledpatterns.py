f8c6544267a59bc6fdf0fa2ce6107e9eff2b3453
Pepijn de Kruijff <p.g.m.dekruijff@student.tudelft.nl>
2020-10-04 02:13:32 +0200

    random colors

--- ledpatterns.py
+++ ledpatterns.py
@@ -189,11 +189,14 @@ if __name__ == '__main__':
             strobe(strip, Color(255, 255, 255))  # white wipe
             strobe(strip, Color(0, 255, 0))  # Blue wipe
             print('Color wipe animations.')
-            colorWipeNoTail(strip, randomColor(), 20, 0, 4)  # random wipe
+            colorWipeNoTail(strip, randomColor(), 20, 1, 3)  # random wipe
+            colorWipeNoTail(strip, randomColor(), 20, 1, 3)  # random wipe
+            colorWipeNoTail(strip, randomColor(), 20, 1, 3)  # random wipe
+            colorWipeNoTail(strip, randomColor(), 20, 1, 3)  # random wipe
 
-            colorWipeNoTail(strip, Color(255, 0, 0), 20, 0, 4)  # Red wipe
-            colorWipeNoTail(strip, Color(0, 255, 0), 20, 0, 4)  # Blue wipe
-            colorWipeNoTail(strip, Color(0, 0, 255), 20, 4)  # Green wipe
+            colorWipeNoTail(strip, Color(255, 0, 0), 20, 1, 3)  # Red wipe
+            colorWipeNoTail(strip, Color(0, 255, 0), 20, 1, 3)  # Blue wipe
+            colorWipeNoTail(strip, Color(0, 0, 255), 20, 1, 3)  # Green wipe
             print('Theater chase animations.')
             theaterChase(strip, Color(127, 127, 127))  # White theater chase
             theaterChase(strip, Color(127,   0,   0))  # Red theater chase
