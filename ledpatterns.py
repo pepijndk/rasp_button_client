a93f6c08318342ad05184643c73c245abc2d9f7f
Pepijn de Kruijff <p.g.m.dekruijff@student.tudelft.nl>
2020-10-04 02:36:04 +0200

    strobeTransition

--- ledpatterns.py
+++ ledpatterns.py
@@ -89,7 +89,7 @@ def strobeTransition(strip, color2, color1=Color(255, 255, 255), wait_ms=40, sec
         # length transition period
         length_transition = int(0.4 * iterations)
         start_transition = int(iterations / 2) - int(length_transition / 2)
-        color_num = i - start_transition / length_transition
+        color_num = (i - start_transition) / length_transition
 
         print("i ", i, "colornum ", color_num)
         if random() <= color_num:
